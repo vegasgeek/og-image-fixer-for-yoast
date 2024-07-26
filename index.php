8445fbc33b9f8501d406603d4a3f64f950d6a7f0
John Hawkins <john@vegasgeek.com>
2024-07-26 07:28:23 -0700

    Added filters for height, width, image type

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  * Plugin Name: Fix OG:Image
  * Description: Uses the featured image as the Open Graph image if one exists.
  * Author: John Hawkins
- * Version: 1.0
+ * Version: 1.0.1
  * Author URI: https://vegasgeek.com
  * Text Domain: fix-og-image
  *
@@ -21,7 +21,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @param string $image The current Open Graph image.
  * @return string The new Open Graph image.
  */
-function alter_existing_opengraph_image( $image ) {
+function vg_alter_og_image( $image ) {
 	$post_id = get_the_ID();
 
 	$featured_image = get_the_post_thumbnail_url( $post_id );
@@ -31,4 +31,73 @@ function alter_existing_opengraph_image( $image ) {
 	}
 	return $image;
 }
-add_filter( 'wpseo_opengraph_image', 'alter_existing_opengraph_image' );
+add_filter( 'wpseo_opengraph_image', 'vg_alter_og_image' );
+
+/**
+ * Filter the Open Graph image width to use the featured image width if one exists.
+ *
+ * @param int $width The current Open Graph image width.
+ * @return int The new Open Graph image width.
+ */
+function vgalter_og_image_width( $width ) {
+	$post_id = get_the_ID();
+
+	$featured_image_id = get_post_thumbnail_id( $post_id );
+
+	if ( $featured_image_id ) {
+		$image_meta = wp_get_attachment_metadata( $featured_image_id );
+		if ( isset( $image_meta['width'] ) ) {
+			$width = $image_meta['width'];
+		}
+	}
+
+	return $width;
+}
+add_filter( 'wpseo_opengraph_image_width', 'vgalter_og_image_width' );
+
+/**
+ * Filter the Open Graph image height to use the featured image height if one exists.
+ *
+ * @param int $height The current Open Graph image height.
+ * @return int The new Open Graph image height.
+ */
+function vg_alter_og_image_height( $height ) {
+	$post_id = get_the_ID();
+
+	$featured_image_id = get_post_thumbnail_id( $post_id );
+
+	if ( $featured_image_id ) {
+		$image_meta = wp_get_attachment_metadata( $featured_image_id );
+		if ( isset( $image_meta['height'] ) ) {
+			$height = $image_meta['height'];
+		}
+	}
+
+	return $height;
+}
+add_filter( 'wpseo_opengraph_image_height', 'vg_alter_og_image_height' );
+
+/**
+ * Filter the Open Graph image type to use the featured image type if one exists.
+ *
+ * @param string $type The current Open Graph image type.
+ * @return string The new Open Graph image type.
+ */
+function vg_alter_og_image_type( $type ) {
+	$post_id = get_the_ID();
+
+	$featured_image_id = get_post_thumbnail_id( $post_id );
+
+	if ( $featured_image_id ) {
+		$image_path = get_attached_file( $featured_image_id );
+		if ( $image_path ) {
+			$image_info = wp_check_filetype( $image_path );
+			if ( isset( $image_info['type'] ) ) {
+				$type = $image_info['type'];
+			}
+		}
+	}
+
+	return $type;
+}
+add_filter( 'wpseo_opengraph_image_type', 'vg_alter_og_image_type' );
